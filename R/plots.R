c2b823da545fcaf91c0040ff85a562c91e9557a1
Lincoln Colling <lincoln@colling.net.nz>
2021-02-10 18:01:59 +0000

    updated plotting predictions

--- R/plots.R
+++ R/plots.R
@@ -1,28 +1,43 @@
  
 #' @export
 plot.bayesplay <- function(x, n = 101) {
-  if (x@dist_type == "point") {
-    data <- data.frame(x = x@parameters$point, y = 1)
-    ggplot2::ggplot(data,
-                    ggplot2::aes(x = x, y = y)) +
-      ggplot2::geom_point(size = 3, shape = 16) +
-      ggplot2::geom_linerange(ggplot2::aes(x = x,
-                                           y = NULL,
-                                           ymax = y,
-                                           ymin = 0)) +
-      ggplot2::xlim(x@plot$range) +
-      ggplot2::labs(x = x@plot$labs$x, y = x@plot$labs$y)
-    } else if (x@dist_type == "continuous") {
+
+  if(("dist_type" %in% slotNames(x)) == TRUE) {
+    if (x@dist_type == "point") {
+      data <- data.frame(x = x@parameters$point, y = 1)
+      ggplot2::ggplot(data,
+                      ggplot2::aes(x = x, y = y)) +
+        ggplot2::geom_point(size = 3, shape = 16) +
+        ggplot2::geom_linerange(ggplot2::aes(x = x,
+                                             y = NULL,
+                                             ymax = y,
+                                             ymin = 0)) +
+        ggplot2::xlim(x@plot$range) +
+        ggplot2::labs(x = x@plot$labs$x, y = x@plot$labs$y)
+      } else if (x@dist_type == "continuous") {
+      ggplot2::ggplot() +
+        ggplot2::geom_function(
+          fun = x@func,
+          colour = "black",
+          na.rm = TRUE,
+          n = 101
+        ) +
+        ggplot2::xlim(x@plot$range) +
+        ggplot2::labs(x = x@plot$labs$x, y = x@plot$labs$y)
+    } else if (x@dist_type == "discrete") {
+
+    }
+   } else {
+    func <- x$prediction_function
     ggplot2::ggplot() +
       ggplot2::geom_function(
-        fun = x@func,
+        fun = func,
         colour = "black",
         na.rm = TRUE,
         n = 101
       ) +
-      ggplot2::xlim(x@plot$range) +
-      ggplot2::labs(x = x@plot$labs$x, y = x@plot$labs$y)
-  } else if (x@dist_type == "discrete") {
+      ggplot2::xlim(x@likelihood_obj@plot$range) +
+      NULL
 
   }
 }
